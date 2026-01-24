b4f9900abfb22a87dd8563246f3a2eaa64d12b90
rar12455 <rarSS@proton.me>
2026-01-24 22:41:04 +0300

    ADDED DOTFILES for rarstatus (old version) and for DWL

---
+++ rarstatus/config.h
@@ -0,0 +1,14 @@
+/* Configuration for rarstatus */
+
+/* Update interval in seconds */
+#define INTERVAL 60
+
+/* Formatting preferences */
+static const bool show_newline = true;
+static const bool no_newline = false;
+
+/* iso_format */
+static const bool iso_format   = true;
+
+/* Separator string */
+static const char *separator = " | ";
